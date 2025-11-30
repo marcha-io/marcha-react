fe8fa5c90a92df75af8c42928e119e667f60daf6
Enrique Jose  Avila Asapche <eavilaasapche@gmail.com>
2025-11-30 12:42:56 +0000

    signing in (#11)

--- src/views/sign_up/SignIn.tsx
+++ src/views/sign_up/SignIn.tsx
@@ -1,66 +1,108 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Flex, Form, Input, Typography } from 'antd';
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Form,
+  Input,
+  Typography,
+  notification,
+} from 'antd';
 import { Header } from 'antd/es/layout/layout';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { supabase } from '../../lib/supabase';
 
 type TSignInForm = {
-  username?: string;
-  password?: string;
+  email: string;
+  password: string;
   remember?: boolean;
 };
 
 const SignIn = (): React.ReactElement => {
-  const onFinish = (form: TSignInForm) => {
-    console.log('Finished', form);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [api, contextHolder] = notification.useNotification();
+
+  const navigate = useNavigate();
+
+  const onFinish = async (form: TSignInForm) => {
+    setIsLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: form.email,
+      password: form.password,
+    });
+    setIsLoading(false);
+
+    if (error != null) {
+      api.error({
+        title: `(${error.name}) Failed to sign in`,
+        description: error.message,
+        duration: 5,
+        pauseOnHover: true,
+      });
+      return;
+    }
+
+    api.success({ title: 'Sign in successful!' });
+    navigate('/');
   };
+
   return (
-    <Flex gap={24} wrap="wrap" align="center" justify="center" vertical>
-      <Header>
-        <Typography.Title>Sign In</Typography.Title>{' '}
-      </Header>
-      <Form
-        size="large"
-        name="login"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your Username or Email',
-            },
-          ]}
+    <>
+      {contextHolder}
+      <Flex gap={24} wrap="wrap" align="center" justify="center" vertical>
+        <Header>
+          <Typography.Title>Sign In</Typography.Title>{' '}
+        </Header>
+        <Form
+          size="large"
+          name="login"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined />} placeholder="Username or Email" />
-        </Form.Item>
-        <Form.Item>
           <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
-            style={{ marginBottom: 0 }}
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email',
+              },
+            ]}
           >
-            <Input
-              prefix={<LockOutlined />}
-              type="password"
-              placeholder="Password"
-            />
+            <Input prefix={<UserOutlined />} placeholder="Email" />
           </Form.Item>
-          <a href="">Forgot password</a>
-        </Form.Item>
+          <Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password' },
+              ]}
+              style={{ marginBottom: 0 }}
+            >
+              <Input
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <a href="">Forgot password</a>
+          </Form.Item>
 
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-        <Form.Item>
-          <Button block type="primary" htmlType="submit">
-            Log in
-          </Button>
-          or <a href="">Register now!</a>
-        </Form.Item>
-      </Form>{' '}
-    </Flex>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit" disabled={isLoading}>
+              Log in
+            </Button>
+            or <a href="">Register now!</a>
+          </Form.Item>
+        </Form>{' '}
+      </Flex>
+    </>
   );
 };
 
